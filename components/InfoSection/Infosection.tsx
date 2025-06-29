5ec6a73144e84527c719c08050d9f10713467270
Martin Lowe <martinlowe673@gmail.com>
2025-06-29 15:28:08 +0100

    updated info section

--- components/InfoSection/Infosection.tsx
+++ components/InfoSection/Infosection.tsx
@@ -1,13 +1,13 @@
 // components/InfoSection.tsx
 const InfoSection: React.FC = () => {
     return (
-      <section className="w-full max-w-4xl p-6 mx-auto my-12 bg-white rounded-lg shadow-md">
-        <h2 className="text-4xl font-bold mb-6 text-center">About Atom Digital</h2>
+      <section className="w-full max-w-4xl p-6 mx-auto my-12 bg-white rounded-lg shadow-md text-blue-500">
+        <h2 className="text-4xl font-bold mb-6 text-center">About Phrehab</h2>
         <p className="text-lg leading-relaxed text-gray-700">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, eveniet.
+         For your physiotherapy needs come to Phrehab
         </p>
         <p className="text-lg leading-relaxed text-gray-700 mt-4">
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, deserunt.
+        Drop us a message or book on the invite below.
         </p>
       </section>
     )
